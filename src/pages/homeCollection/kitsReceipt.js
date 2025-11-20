fb40a27640d9920cb59b751e60257f25256208ca
Gene Barra <gebarra9@gmail.com>
2025-11-20 16:00:35 -0500

    update partial loss text

--- src/pages/homeCollection/kitsReceipt.js
+++ src/pages/homeCollection/kitsReceipt.js
@@ -52,7 +52,7 @@ const kitsReceiptTemplate = async (name) => {
                             <option id="select-pkgCrushed" value=${conceptIds.pkgCrushed}>Package Crushed</option>
                             <option id="select-pkgImproperPackaging" value=${conceptIds.pkgImproperPackaging}>Improper Packaging</option>
                             <option id="select-pkgCollectionCupDamaged" value=${conceptIds.pkgCollectionCupDamaged}>Collection Cup Damaged</option>
-                            <option id="pkgCollectionCupLeakedPartialLoss" value=${conceptIds.pkgCollectionCupLeakedPartialLoss}>Collection Cup Leaked - Partial Loss</option>
+                            <option id="pkgCollectionCupLeakedPartialLoss" value=${conceptIds.pkgCollectionCupLeakedPartialLoss}>Collection Cup Leaked - Partial Sample Loss</option>
                             <option id="pkgCollectionCupLeakedTotalLoss" value=${conceptIds.pkgCollectionCupLeakedTotalLoss}>Collection Cup Leaked - Total Sample Loss</option>
                             <option id="select-pkgEmptyCupReturned" value=${conceptIds.pkgEmptyCupReturned}>Empty Cup Returned</option>
                             <option id="select-pkgIncorrectMaterialType" value=${conceptIds.pkgIncorrectMaterialType}>Incorrect Material Type</option>
