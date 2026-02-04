453e19459463c507ea24ef5771e910601cfa0a45
anthonypetersen <tonypetersen@hotmail.com>
2026-02-04 16:16:32 -0600

    update

--- src/events.js
+++ src/events.js
@@ -1971,9 +1971,9 @@ export const addEventSaveButton = async (boxIdAndBagsObj) => {
 }
 
 const validateShipperEmail = (email) => {
-    const finalizedSignInput = escapeHTML(document.getElementById('finalizeSignInput').value);
+    const finalizedSignInput = document.getElementById('finalizeSignInput').value;
     if (finalizedSignInput.toUpperCase() !== email.toUpperCase()) {
-        showNotifications({ title: 'Error Shipping Box(es)', body: `Email mismatch. You entered: ${finalizedSignInput}, which does not match the email on record.` });
+        showNotifications({ title: 'Error Shipping Box(es)', body: `Email mismatch. You entered: ${escapeHTML(finalizedSignInput)}, which does not match the email on record.` });
         return false;
     }
     return true;
